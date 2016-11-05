50dc76be77cbe35f1a4aab49aa69414d59e7555c
wongpet5 <peterskwongut@gmail.com>
2016-11-05 16:45:32 -0400

    Middle ware updates

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
+var moment = require('moment'); 
+var expressValidator = require('express-validator'); 
+
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +29,43 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Express session
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}))
+
+// Express validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect-Flash
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+app.use(function(req, res, next){
+  req.db = db;
+  next(); 
+})
 
 app.use('/', routes);
 app.use('/users', users);
