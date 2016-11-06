51c89ae4cc2b74414e1be785765e2bbced16e03f
wongpet5 <peterskwongut@gmail.com>
2016-11-06 09:16:01 -0500

    Add Post
    Added the Ability to Add Posts to blog

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var multer = require('multer');
 var upload = multer({ dest: 'uploads/' })
-var moment = require('moment'); 
 var expressValidator = require('express-validator'); 
 
 var mongo = require('mongodb');
 var db = require('monk')('localhost/nodeblog');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var posts = require('./routes/posts');
 
 var app = express();
+
+app.locals.moment = require('moment'); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -68,7 +69,7 @@ app.use(function(req, res, next){
 })
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/posts', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
